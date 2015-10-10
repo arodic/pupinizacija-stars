7d6bb5cfcffc7d2e7ccd5bfcf2629f50deaa4df1
Aki Rodic <aleksandar.xyz@gmail.com>
2015-10-10 11:57:43 +0800

    Added web socket 15fps packet limit and added animation smoothing on client side

--- serial.js
+++ serial.js
@@ -3,6 +3,26 @@ var WebSocketServer = require('ws').Server
 
 var client_ws, data, value;
 
+var ready = false;
+
+var animate = function() {
+
+  // Debug
+  // try {
+  //   wss.clients.forEach(function each(client) {
+  //     client.send(String(128 + 64 *  Math.sin(Date.now() / 1000)));
+  //   });
+  // }
+  // catch(err) {
+  //   console.log(err);
+  // }
+
+  ready = true;
+  setTimeout(animate, 1000/15);
+}
+
+animate();
+
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/tty.usbserial-AI02NIF1", {
   baudrate: 256000
@@ -11,8 +31,11 @@ var serialPort = new SerialPort("/dev/tty.usbserial-AI02NIF1", {
 serialPort.on("open", function () {
   console.log('Serial port opened.');
   serialPort.on('data', function(data) {
+    if (!ready) return;
+    ready = false;
     data = data.toJSON().data;
-    value = String(data[data.length - 1]);
+    // value = String(data[data.length - 1]);
+    value = String(data[0]);
     try {
       wss.clients.forEach(function each(client) {
         client.send(value);
